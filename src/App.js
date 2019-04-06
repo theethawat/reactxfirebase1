1fcbf32703488316a70b64f690d6dc11aefcfd0e
Theethawat <theethawat.s@outlook.co.th>
2019-04-07 00:30:04 +0700

    Updating Include firebase RTDB

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+let config = {
+  apiKey: "AIzaSyCRsfG6PU2w3nqIQDov0MYO-4nYsAb7FvI",
+  authDomain: "fir-politic.firebaseapp.com",
+  databaseURL: "https://fir-politic.firebaseio.com",
+  projectId: "fir-politic",
+  storageBucket: "fir-politic.appspot.com",
+  messagingSenderId: "212092087992"
+};
+firebase.initializeApp(config);
+let db = firebase;
+
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       political: [
-        {name:'',nickname:'',party:''}
+        { name: '', nickname: '', party: '', age: '' }
       ],
     }
-  }
+    let political = db.database().ref("politician")
+    political.on('value', snapshot => {
+      snapshot.forEach(childSnapshot => {
+        let childKey = childSnapshot.key
+        let childValue = childSnapshot.val()
+        console.log(childKey)
+        console.log(childValue)
 
-  componentDidMount() {
-    this.setState({
-      political: [
-        {name: 'Abhisit', nickname: 'Mark', party: 'Democrat'},
-        {name:'Sudarat',nickname:'Nhoi',party:'PhuerThai'}
-      ]
-    })
-  }
+        this.setState({
+          political: [childValue]
+        })
+      })
+    })}
+
+
+
+  // componentDidMount() {
+  //   
+  // }
 
   render() {
-  let political = this.state.political
-  let politicInfo = political.map((member) => 
-    <li>Name: {member.name}  Nickname: {member.nickname}  Party: {member.party}</li>
-    )
-    return (
+        let political = this.state.political
+
+    let politicInfo = political.map((member) =>
+          <li><b>Name:</b> {member.name}  <b>Nickname:</b> {member.nickname}  <b>Party:</b> {member.party} <b>Age:</b> {member.age}</li>
+        )
+    return(
       <div>
-        <h3>Political People: {politicInfo} </h3>
-      </div>
+      <h3>Political People: </h3>
+        { politicInfo }
+      </div >
     );
   }
 }
